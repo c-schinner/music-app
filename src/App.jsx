56c3fae2877c24689fd097137625673b2263ca91
Chris <cschinner7@gmail.com>
2024-09-09 14:36:04 -0700

    update App.jsx
    cleaned App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ function App() {
 
   return (
     <>
-      <h1>This is our new music app</h1>
-      <p>To create playlists thru spotify API</p>
-      <Header />
+    <div>
+        <Header />
+      </div>
     </>
   )
 }
