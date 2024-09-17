76cad1def91ea30c9fa487d34bdd9f4106f77c2a
Chris <cschinner7@gmail.com>
2024-09-17 09:46:58 -0700

    App adjustments
    made adjustments to the layout for better mobile design. and removed unused file.

--- src/components/MusicList.jsx
+++ src/components/MusicList.jsx
@@ -4,18 +4,18 @@ const MusicList = ({ songs, addToPlaylist }) => {
 
     return (
         <>
-            <div className="h-full w-1/2 mx-auto text-white">
+            <div className="w-full sm:w-1/2 mx-auto text-white p-4">
                 <h2 className="text-2xl font-semibold mb-4">Music List:</h2>
                 {songs.length > 0 ? (
                     <ul className="space-y-4">
                         {songs.map((song) => (
-                            <li key={song.id} className="border-b pb-2 flex items-center space-x-4">
+                            <li key={song.id} className="border-b pb-2 flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4">
                                 <img 
                                 src={song.album.images[0].url}
                                 alt={song.album.name}
                                 className="w-16 h-16 object-cover rounded"
                             />
-                                <div className="flex-grow">
+                                <div className="flex-grow text-center sm:text-left">
                                     <p className="text-lg font-bold">{song.name}</p>
                                     <p className="text-sm">{song.artists.map(artist => artist.name).join(', ')}</p>
                                     <p className="text-sm">{song.album.name}</p>
--- src/components/PlayList.jsx
+++ src/components/PlayList.jsx
@@ -3,18 +3,18 @@ const PlayList = ({ playlist, removeFromPlaylist }) => {
 
     return (
         <>
-            <div className="h-full w-1/2 mx-auto text-white ml-10">
+            <div className="w-full sm:w-1/2 mx-auto text-white ml-10 p-4">
             <h2 className="text-2xl font-semibold mb-4">Play List:</h2>
             {playlist.length > 0 ? (
                 <ul className="space-y-4">
                     {playlist.map((song) => (
-                        <li key={song.id} className="border-b pb-2 flex items-center space-x-4">
+                        <li key={song.id} className="border-b pb-2 flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4">
                             <img 
                             src={song.album.images[0].url}
                             alt={song.album.name}
                             className="w-16 h-16 object-cover rounded"
                         />
-                            <div className="flex-grow">
+                            <div className="flex-grow text-center sm:text-left">
                                 <p className="text-lg font-bold">{song.name}</p>
                                 <p className="text-sm">{song.artists.map(artist => artist.name).join(', ')}</p>
                                 <p className="text-sm">{song.album.name}</p>
